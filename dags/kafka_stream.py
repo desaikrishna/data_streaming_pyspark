05aa6b65f4245c11afc898b418bac390da56de65
desaikrishna <desaikrishna007@gmail.com>
2024-09-30 23:36:22 +0530

    data fetch and scheduler code

---
+++ dags/kafka_stream.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+
+    'owner': 'desaikrishna',
+    'start_date': datetime(2024,9,2,10,00)
+}
+
+def get_data():
+    import json
+    import requests
+
+    res = requests.get("https://randomuser.me/api")
+    res=res.json()
+    res=res['results'][0]
+    return res
+    #print(json.dumps(res,indent=3))
+
+
+def format_data(res):
+    data={}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_nmae'] = res['name']['last']
+    data['gender'] = res['gender']
+    #data['address'] = str(location['street']['number']) + ' ' + location['street']['name'] + ', ' + location['city'] + ', ' + 
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    import time, logging
+
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000) # local ip of kafka broker
+    curr_time=time.time()
+
+    while True:
+        if time.time() > curr_time + 60:
+            break
+        try:
+            res= get_data()
+            res= format_data(res)
+
+            producer.send('users_created',json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+    
+
+    # try:
+    #     res= get_data()
+    #     res= format_data(res)
+
+    #     producer.send('users_created',json.dumps(res).encode('utf-8'))
+    # except Exception as e:
+    #     logging.error(f'An error occured: {e}')
+
+
+    
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+    
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
+
+#get_data()
+#format_data()
+#stream_data()
+#you  just called one function but all three functions worked, which was dependent of one each check why
